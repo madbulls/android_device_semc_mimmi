5027b51af5c4c2e46e24be06484c2314bbe55916
madbulls <madbulls01@gmail.com>
2013-03-17 22:04:24 +0700

    cleanup

--- MimmiParts/src/com/cyanogenmod/settings/device/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.cyanogenmod.settings.device;
-
-/**
- * User: Pedro Veloso pedronveloso@androidpt.com
- */
-public interface Constants {
-    
-    public static String LOG_TAG = "mimmi_parts";
-}
